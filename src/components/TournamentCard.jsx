0663c7241461e2b2be9006470d2a384915303049
Hamza <hamzazaidi253@gmail.com>
2026-03-11 12:02:05 +0000

    ajouter des composant

--- src/components/TournamentCard.jsx
+++ src/components/TournamentCard.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import StatusBadge from "./StatusBadge";
 
 export default function TournamentCard({ tournament }) {
+    const [isRegistered, setIsRegistered] = useState(false)
+
   return (
     <div className="bg-white p-4 rounded-xl shadow mt-4">
 
@@ -13,6 +16,15 @@ export default function TournamentCard({ tournament }) {
       <p className="text-gray-500 text-sm mt-2">
         {tournament.description}
       </p>
+
+      <button
+          onClick={()=>setIsRegistered(!isRegistered)}
+        className={isRegistered ? "bg-red-500 text-white p-2" : "bg-green-500 text-white p-2  rounded-[20px] "}
+>
+    {isRegistered ? "Se désinscrire" : "S'inscrire"}
+
+
+</button>
 
       <div className="text-sm mt-3 space-y-1 text-gray-600">
         <p>👥 {tournament.participants.length} participants</p>
