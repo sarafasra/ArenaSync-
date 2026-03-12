f58e70739221132ba2e17e1293d61ad5ffe619bd
Hamza <hamzazaidi253@gmail.com>
2026-03-12 10:30:10 +0000

    ajouter quelque modification sur composant app

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,18 @@ import { tournamentData } from "./data/tournamentDB.jsx";
 import RegistrationForm from "./components/RegistrationForm.jsx";
 
 function App() {
+
+
+  const [participants, setParticipants] = useState([]);
+
+ 
+  const addParticipant = (participant) => {
+    setParticipants([...participants, participant]);
+  };
+
   return (
     <div>
       <Header />
-
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
         {tournamentData.map((tournament) => (
@@ -18,24 +26,25 @@ function App() {
             tournament={tournament}
           />
         ))}
-        <div>
-
-      <RegistrationForm onSubmit={addParticipant} />
-
-      <h2>Liste des participants</h2>
-      <ul>
-        {participants.map((p, i) => (
-          <li key={i}>
-            {p.name} - {p.equipe} - {p.niveau}
-          </li>
-        ))}
-      </ul>
-    </div>
       </div>
 
-        
+      <div>
+
+        <RegistrationForm onSubmit={addParticipant} />
+
+        <h2>Liste des participants</h2>
+
+        <ul>
+          {participants.map((p, i) => (
+            <li key={i}>
+              {p.name} - {p.equipe} - {p.niveau}
+            </li>
+          ))}
+        </ul>
+
+      </div>
+
     </div>
-    
   );
 }
 
